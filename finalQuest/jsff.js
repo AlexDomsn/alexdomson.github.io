cd0efd0441b177b9654e24c4c1495f888bee1ef6
AlexD <alex@4ats.ru>
2016-08-22 23:46:27 +0300

    finalQuest2

--- finalQuest/jsff.js
+++ finalQuest/jsff.js
@@ -6,18 +6,42 @@ $(window).on('load', function () {
 });
 // Прелоадер
 
+
+function getAge(birthStr) {
+    //
+    // var year = parseInt(birthStr.substr(0, 4))
+    // var age = new Date().getFullYear() - year
+    // return age
+
+    var year=parseInt(birthStr.substr(0, 4));
+    var month=parseInt(birthStr.substr(5, 2))-1;
+    var day=parseInt(birthStr.substr(8, 2));
+    var today=new Date();
+    var age=today.getFullYear()-year;
+    if(today.getMonth()<month || (today.getMonth()==month && today.getDate()<day)){age--;}
+    return age;
+}
+
 $(function(){
     $.getJSON('echo.json', function(data) {
-        for(var i=0;i<data.values.length;i++){
+        for( var  i=0;i<data.values.length;i++){
+            var age = getAge(data.values[i].birthDate.v);
+            var year = data.values[i].birthDate.v.substr(0, 4);
+            var month = data.values[i].birthDate.v.substr(5, 2);
+            var day = data.values[i].birthDate.v.substr(8, 2);
             $('#users').append('<tr><td>' + data.values[i].name.v + '</td>' +
                 '<td>' + data.values[i].firstName.v + '</td>' +
-                '<td>' + data.values[i].birthDate.v + '</td>' +
+                '<td>' +  age + '(' + year + '-' + month + '-' + day + ')' + '</td>' +
                 '<td><a href="tel:'+ data.values[i].tel.v + '" class="ng-binding">' + data.values[i].tel.v + '</a></td>' +
                 '<td><a target="_blank"  href="http://maps.google.com/?q=' + data.values[i].address.v + '">'  + data.values[i].address.v + '</a></td></tr>');
-            $('.spc').append('<caption>' + data.values.length + '</caption>')
+
+
         }
+        $('.spc').append('<caption>' + data.values.length + '</caption>')
+
     });
 });
+
 
 // Формирование таблицы из файла json
 
